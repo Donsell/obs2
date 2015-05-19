88a0499365be936f78d5b608fddffba1049a6de5
Dan Delzell <dan@delzell.net>
2015-05-19 11:39:54 -0500

    added generator fix

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,7 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
-
+require 'rails/generators'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -36,7 +36,6 @@ module Obs2
     # For Foundation 5
     config.assets.precompile += %w( vendor/modernizr )
 
-    config.middleware.use PDFKit::Middleware
 
   end
 end
