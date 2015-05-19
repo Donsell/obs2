5358ab0eca443f422d345282ea2d62f0b81e3764
Dan Delzell <dan@delzell.net>
2015-05-19 14:40:59 -0500

    updated ruby version

--- config/deploy.rb
+++ config/deploy.rb
@@ -14,7 +14,7 @@ set :repo_url, 'https://github.com/Donsell/obs2.git'
 # Default value for :scm is :git
 set :scm, :git
 set :rbenv_type, :system
-set :rbenv_ruby, '2.0.0'
+set :rbenv_ruby, '2.0.0-p481'
 set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
 set :rbenv_map_bins, %w{rake gem bundle ruby rails}
 
