4fc37f1b93d24502f034ce38ddaac3de43d4622b
Dan Delzell <dan@delzell.net>
2015-05-20 15:20:07 -0500

    moved require rails all to top of apprb

--- config/application.rb
+++ config/application.rb
@@ -1,8 +1,8 @@
 require File.expand_path('../boot', __FILE__)
 
+require 'rails/all'
 require 'foundation-rails'
 require 'rails/generators'
-require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
