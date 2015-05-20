ce20f7284549e84433629eb6d56282ef28efb9cb
Dan Delzell <dan@delzell.net>
2015-05-20 15:24:31 -0500

    more apprb changes

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,7 @@
 require File.expand_path('../boot', __FILE__)
 
-require 'rails/all'
 require 'foundation-rails'
+require 'rails/all'
 require 'rails/generators'
 
 # Require the gems listed in Gemfile, including any gems
