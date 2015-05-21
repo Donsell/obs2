29f794801602170fd69f3602aeb647e716085f17
Dan Delzell <dan@delzell.net>
2015-05-21 09:25:01 -0500

    removed stat_id from users

--- db/migrate/20150508231315_add_stats_to_users.rb
+++ db/migrate/20150508231315_add_stats_to_users.rb
@@ -1,6 +1,6 @@
 class AddStatsToUsers < ActiveRecord::Migration
   def change
-    remove_column :users, :stat_id
+    #remove_column :users, :stat_id
     add_column :users, :obs, :integer
     add_column :users, :sites_obs, :text
     add_column :users, :prog_obs, :text
