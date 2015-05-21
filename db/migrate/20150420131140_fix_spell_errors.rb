0dea98670fc55a2228fd1c651cc74205da21527b
Dan Delzell <dan@delzell.net>
2015-05-21 09:17:33 -0500

    removed drop table sessions

--- db/migrate/20150420131140_fix_spell_errors.rb
+++ db/migrate/20150420131140_fix_spell_errors.rb
@@ -2,6 +2,5 @@ class FixSpellErrors < ActiveRecord::Migration
   def change
     rename_column :outings, :limiting_magnatude, :limiting_magnitude
     rename_column :outings, :transparancy, :transparency
-    drop_table :sessions
   end
 end
