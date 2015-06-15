92db79a7768e9f170b87d86d141240d22162a5f9
Dan Delzell <dan@delzell.net>
2015-06-15 10:00:41 -0500

    Findished Double Stars

--- app/models/site.rb
+++ app/models/site.rb
@@ -1,6 +1,7 @@
 class Site < ActiveRecord::Base
   has_many :outings
-
+  validates :name, presence: true
+  before_validation :check_name
   LOCATION_TYPES=
   {
     1 => "Urban",
@@ -8,5 +9,9 @@ class Site < ActiveRecord::Base
     3 => "Rural",
     4 => "Dark Sky"
   }
+
+  def check_name
+    errors.add("Site Name must exist.") if self.name.nil?
+  end
 
 end
